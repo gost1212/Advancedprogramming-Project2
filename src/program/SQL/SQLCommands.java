420a9ae8aed80c4e84a2b6d8574586c860e49f69
Abod019 <98422460+Abod019@users.noreply.github.com>
2023-06-12 15:08:33 +0300

    Update SQLCommands.java

--- src/program/SQL/SQLCommands.java
+++ src/program/SQL/SQLCommands.java
@@ -66,9 +66,78 @@ public class SQLCommands {
                                                        select count(*) from `productstbl_saad_abdullah`;
                                                        """;//used to know how many entries did we get.
     
-    public static ResultSet searchByTypeModel(String typeModel){
-        return null;
+   protected static void searchByTypeModel(String typeModel, TableView<Product> tableView, Label statusLabel) {
+
+    PreparedStatement selectStatement = null; // Preparing.
+    PreparedStatement selectLength = null;
+    ResultSet result = null;
+    int length = 0;
+    String statementSQL = ""; // Used as a way select routine.
+    String statementLengthSQL = ""; // Used as a way select routine.
+
+    if (typeModel.isEmpty()) { // If no input received.
+        statementSQL = SELECT_ALL_STATMENT;
+        statementLengthSQL = SELECT_ALL_LENGTH_STATMENT;
+    } else {
+        statementSQL = SELECT_TYPE_MODEL_STATMENT;
+        statementLengthSQL = SELECT_TYPE_MODEL_LENGTH_STATMENT;
     }
+
+    try { // Prepare statements.
+        selectStatement = conn.prepareStatement(statementSQL);
+        selectLength = conn.prepareStatement(statementLengthSQL);
+        
+        if (!typeModel.isEmpty()) { // Checks if there's any input before adding parameters
+            selectStatement.setString(1, "%" + typeModel + "%");
+            selectStatement.setString(2, "%" + typeModel + "%");
+            selectLength.setString(1, "%" + typeModel + "%");
+            selectLength.setString(2, "%" + typeModel + "%");
+        }
+
+        result = selectStatement.executeQuery();
+        ResultSet temp = selectLength.executeQuery();
+        temp.next();
+        length = temp.getInt(1);
+        temp.close();
+        
+        ObservableList<Product> productList = FXCollections.observableArrayList();
+        while (result.next()) {
+            int id = result.getInt("id");
+            String type = result.getString("Type");
+            String model = result.getString("Model");
+            float price = result.getFloat("Price");
+            int count = result.getInt("Count");
+            Date deliveryDate = result.getDate("DeliveryDate");
+            productList.add(new Product(id, type, model, price, count, deliveryDate));
+        }
+        
+        tableView.setItems(productList);
+        
+        if (length == 0 && typeModel.length() > 0) {
+            statusLabel.setTextFill(Color.RED);
+            statusLabel.setText("No records available for this search criteria!");
+        } else {
+            statusLabel.setText("");
+        }
+
+    } catch (SQLException e) {
+        System.out.println("Failed to get values. Error: " + e.getMessage());
+        statusLabel.setTextFill(Color.RED);
+        statusLabel.setText("Error: " + e.getMessage());
+    } finally {
+        try {
+            if (selectStatement != null) {
+                selectStatement.close();
+            }
+            if (result != null) {
+                result.close();
+            }
+        } catch (SQLException e) {
+            System.out.println("Failed to close statement or result set. Error: " + e.getMessage());
+        }
+    }
+}
+
 
     private final static String DELETE_ID_STATMENT = """
                                                      delete from productstbl_saad_abdullah where id = ?;
